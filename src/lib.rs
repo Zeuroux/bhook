addad972044fcf73cdda06b8edce28675a8cc983
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2025-06-21 10:08:33 -0300

    Crate

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+#![allow(clippy::unsafe_op_in_unsafe_fn)]
+use clear_cache::clear_cache;
+use core::mem::transmute;
+use core::ptr;
+use region::{Protection, protect_with_handle};
+#[derive(Debug)]
+enum HookingError {
+    MemoryProtection(region::Error),
+}
+mod raw;
+pub use raw::BACKUP_LEN;
+use raw::hook_impl;
+/// Branch hook a function.
+/// This function will write a basic hook in the memory you give it, \
+/// differently from other hooking libraries, the method is very crude, as its literally \
+/// just a branch instruction to 'hook_fn', this has the benefit of not needing high complexity \
+/// at the cost of not being able to make a trampoline
+#[inline(always)]
+pub unsafe fn raw_hook(target: *mut u8, hook_fn: usize) {
+    unsafe { hook_impl(target, hook_fn) }
+}
+/// Handle memory protection and save backup bytes before doing branch hooking.
+/// You can use 'unsetup_hook' to call the original function using this
+pub unsafe fn setup_hook(
+    orig_fn: *mut u8,
+    hook_fn: *const u8,
+) -> Result<[u8; BACKUP_LEN], region::Error> {
+    unsafe {
+        #[cfg(not(target_arch = "arm"))]
+        let offset_fn = orig_fn;
+        #[cfg(target_arch = "arm")]
+        let offset_fn = orig_fn.offset(-1);
+        let _handle = protect_with_handle(offset_fn, BACKUP_LEN, Protection::READ_WRITE_EXECUTE)?;
+        let result = ptr::read_unaligned(offset_fn as *mut [u8; BACKUP_LEN]);
+        raw_hook(orig_fn, hook_fn as usize);
+        clean_cache(offset_fn as *const u8, BACKUP_LEN);
+        Ok(result)
+    }
+}
+
+// Handle memory protection and then write back the backup bytes.
+pub unsafe fn unsetup_hook(
+    orig_fn: *mut u8,
+    orig_code: [u8; BACKUP_LEN],
+) -> Result<(), region::Error> {
+    unsafe {
+        #[cfg(target_arch = "arm")]
+        let orig_fn = orig_fn.offset(-1);
+        let _handle = protect_with_handle(orig_fn, BACKUP_LEN, Protection::READ_WRITE_EXECUTE)?;
+        ptr::write_unaligned(orig_fn as *mut [u8; BACKUP_LEN], orig_code);
+        clean_cache(orig_fn as *const u8, BACKUP_LEN);
+    }
+    Ok(())
+}
+
+#[inline(always)]
+unsafe fn clean_cache(ptr: *const u8, len: usize) -> bool {
+    unsafe { clear_cache(ptr, ptr.add(len)) }
+}
+
+#[macro_export]
+macro_rules! hook_fn {
+    (fn $name:ident($($arg_name:ident : $arg_ty:ty),*) -> $ret_type:ty = $body:expr )  => {
+        mod $name {
+            static mut CONTEXT: Option<HookCtx> = None;
+            struct HookCtx {
+                original: *mut u8,
+                hook: *const u8,
+                backup: [u8; bhook::BACKUP_LEN]
+            }
+                pub unsafe fn hook_address(original_addr: *mut u8) {
+                    unsafe {
+                    let backup = bhook::setup_hook(original_addr, mainhook as *const u8);
+                    CONTEXT = Some(HookCtx {
+                        original: original_addr,
+                        hook: mainhook as *const u8,
+                        backup: backup.unwrap(),
+                    })
+                    }
+                }
+
+
+            unsafe fn call_original($($arg_name : $arg_ty), *) -> $ret_type {
+                unsafe {
+                    let sus = CONTEXT.as_ref().unwrap();
+                    bhook::unsetup_hook(sus.original, sus.backup).unwrap();
+                    let original = core::mem::transmute::<*mut u8, fn($($arg_ty),*) -> $ret_type>(sus.original);
+                    let result = original($($arg_name),*);
+                    bhook::setup_hook(sus.original, sus.hook).unwrap();
+                    result
+                }
+            }
+            unsafe extern "C" fn mainhook( $($arg_name: $arg_ty),*) -> $ret_type {unsafe {$body }}
+            // unsafe extern "C" fn hook($($arg_name: $arg_ty),*) -> $ret_type {
+            //     mainhook( $($arg_name),*)
+            // }
+        }
+    };
+
+    (fn $fnname:ident($($arg_name:ident : $arg_ty:ty),*) = $body:expr )  => {
+
+    };
+    // (fn $fnname:ident($($arg_name:ident : $arg_ty:ty),*) { $body:expr })  => {
+
+    // };
+}
---
+++ src/raw.rs
@@ -0,0 +1,81 @@
+use core::ptr;
+#[cfg(target_arch = "aarch64")]
+// Magic value: code len (8) + pointer length (8)
+pub const BACKUP_LEN: usize = 16;
+#[cfg(target_arch = "aarch64")]
+pub unsafe fn hook_impl(target: *mut u8, hook_fn: usize) {
+    unsafe {
+        const CODE: [u8; 8] = [
+            0x43, 0x00, 0x00, 0x58, // ldr x3, +0x8
+            0x60, 0x00, 0x1f, 0xd6, // br x3
+        ];
+        const CODE_USIZE: usize = usize::from_ne_bytes(CODE);
+        ptr::write(target as *mut [usize; 2], [CODE_USIZE, hook_fn]);
+    }
+}
+#[cfg(target_arch = "arm")]
+fn is_thumb(addr: u32) -> bool {
+    addr & 1 != 0
+}
+#[cfg(target_arch = "arm")]
+fn clear_thumb_bit(addr: u32) -> u32 {
+    addr & 0xfffffffe
+}
+#[cfg(target_arch = "arm")]
+fn is_aligned(addr: u32) -> bool {
+    addr % 4 == 0
+}
+#[cfg(target_arch = "arm")]
+// Magic value: code len (4) + pointer length(4) + align(1)
+pub const BACKUP_LEN: usize = 9;
+#[cfg(target_arch = "arm")]
+pub unsafe fn hook_impl(target: *mut u8, hook_fn: usize) {
+    let target_addr = target as u32;
+    if is_thumb(target_addr) {
+        // asm: nop
+        const THUMB_NOOP: u16 = 0xbf00;
+        // asm: ldr.w pc, [pc]
+        const LDR_PC_PC: [u16; 2] = [0xf8df, 0xf000];
+        let target_addr = clear_thumb_bit(target_addr);
+        let mut target = target_addr as *mut u16;
+        if !is_aligned(target_addr) {
+            *target = THUMB_NOOP;
+            target = target.offset(1);
+        }
+        *(target as *mut [u16; 2]) = LDR_PC_PC;
+        *(target.offset(2) as *mut usize) = hook_fn;
+    } else {
+        // asm: ldr pc, [pc, -4]
+        const CODE: usize = 0xe51ff004;
+        let arm_insns = target_addr as *mut usize;
+        *arm_insns = CODE;
+        *arm_insns.offset(1) = hook_fn;
+    }
+}
+
+#[cfg(target_arch = "x86_64")]
+pub const BACKUP_LEN: usize = 12;
+#[cfg(target_arch = "x86_64")]
+pub unsafe fn hook_impl(target: *mut u8, hook_fn: usize) {
+    let mut code: [u8; 12] = [
+        0x48, 0xb8, // movabs rax, <ptr>
+        0, 0, 0, 0, 0, 0, 0, 0, // <ptr>, we copy the real ptr later
+        0xff, 0xe0, // jmp rax
+    ];
+    code[2..10].copy_from_slice(&hook_fn.to_ne_bytes());
+    (target as *mut [u8; 12]).write(code);
+}
+
+#[cfg(target_arch = "x86")]
+// Magic value: mov length (1) + pointer length (4) + jmp len (2)
+pub const BACKUP_LEN: usize = 7;
+#[cfg(target_arch = "x86")]
+pub unsafe fn hook_impl(target: *mut u8, hook_fn: usize) {
+    let mut code: [u8; 7] = [
+        0xB8, // mov eax, <ptr>
+        0, 0, 0, 0, // <ptr>, we copy the real ptr later
+        0xFF, 0xE0, // jmp eax
+    ];
+    code[1..5].copy_from_slice(&hook_fn.to_ne_bytes());
+    (target as *mut [u8; 5]).write(code);
+}
